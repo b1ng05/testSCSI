07626dc6fd1dcdf7e004718e4a15980c4c9af5f0
b1ng05 <benysbils@gmail.com>
2024-07-24 15:25:47 +0100

    Update the TBM IOCTL code

--- linux_commands_ioctl_IBM.cpp
+++ linux_commands_ioctl_IBM.cpp
@@ -12,15 +12,15 @@
 #include <linux/version.h>
 #include "/home/prouser/Videos/lin_tape-3.0.66/IBM_tape.h"
 
-int main() {
+int main() { 
     int fd = open("/dev/IBMtape0", O_RDONLY);
-    if (fd < 0) {
+    if (fd < 0) { 
         std::cerr << "Failed to open tape device: " << strerror(errno) << std::endl;
         return 1;
     }
-
+    
     std::cout << "LINUX_VERSION_CODE: " << LINUX_VERSION_CODE << std::endl;
-
+    
     // Unload tape command using MTIOCTOP
     struct mtop tape_operation;
     tape_operation.mt_op = MTOFFL; // Unload the tape
@@ -32,26 +32,7 @@ int main() {
         return 1;
     }
 
-    // Query tape parameters using STIOCQRYP
-    struct stchgp_s stchgp;
-    if (ioctl(fd, STIOCQRYP, &stchgp) < 0) {
-        std::cerr << "ioctl STIOCQRYP failed: " << strerror(errno) << std::endl;
-        close(fd);
-        return 1;
-    }
-
-    std::cout << "Current tape parameters:" << std::endl;
-    std::cout << "rewind_immediate: " << (stchgp.rewind_immediate ? "true" : "false") << std::endl;
-    std::cout << "trailer_labels: " << (stchgp.trailer_labels ? "true" : "false") << std::endl;
-
-    // Set new tape parameters using STIOCSETP
-    stchgp.rewind_immediate = 1;
-    stchgp.trailer_labels = 1;
-    if (ioctl(fd, STIOCSETP, &stchgp) < 0) {
-        std::cerr << "ioctl STIOCSETP failed: " << strerror(errno) << std::endl;
-        close(fd);
-        return 1;
-    }
+    std::cout << "Cartridge successfully unloaded." << std::endl;
 
     close(fd);
     return 0;
